17fe2fc74cebde429b8bfbf1da76b6b105860df8
Sequentz <wouterkvandenbossche@gmail.com>
2024-09-11 17:15:12 +0200

    made controllers

---
+++ app/Http/Controllers/Api/MuscleGroupApiController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MuscleGroup;
+use Illuminate\Http\Request;
+
+class MuscleGroupApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MuscleGroup $muscleGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MuscleGroup $muscleGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MuscleGroup $muscleGroup)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/SchemeApiController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Scheme;
+use Illuminate\Http\Request;
+
+class SchemeApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Scheme $scheme)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Scheme $scheme)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Scheme $scheme)
+    {
+        //
+    }
+}
